bcebe2cea3d81677bd50073ad01665e90604507e
Alexander Chadin <joker946@gmail.com>
2015-08-20 13:52:26 +0300

    min update

--- etc/raw_to_stats/min.py
+++ etc/raw_to_stats/min.py
@@ -1,8 +1,11 @@
 import json
 import dateutil.parser
 import math
+import pprint
 
 from copy import deepcopy
+pp = pprint.PrettyPrinter(depth=3)
+
 
 def calculate_sd(hosts, param):
     mean = reduce(lambda res, x: res + hosts[x][param],
@@ -14,6 +17,7 @@ def calculate_sd(hosts, param):
     sd = math.sqrt(variaton)
     #LOG.debug("SD %(param)s: %(sd)f", {'sd': sd, 'param': param})
     return sd
+
 
 
 def calculate_cpu(instance, compute_nodes=None):
@@ -69,7 +73,7 @@ instances = data[0]['instances']
 compute_nodes = data[1]['compute_nodes']
 a = fill_compute_stats(instances, compute_nodes)
 a = [calculate_host_loads(compute_nodes, a)]
-print a
+pp.pprint(a)
 with open('data.json', 'w') as output:
     json.dump(a, output)
 
@@ -96,7 +100,7 @@ def _simulate_migration(instance, node, host_loads, compute_nodes):
 def min_sd(**kwargs):
     compute_nodes = kwargs.get('nodes')
     host_loads = fill_compute_stats(instances, compute_nodes)
-    print host_loads
+    pp.pprint(host_loads)
     vm_host_map = []
     for instance in instances:
         for node in compute_nodes:
@@ -109,6 +113,20 @@ def min_sd(**kwargs):
                                     'vm': instance['instance_uuid'],
                                     'sd': sd})
     vm_host_map = sorted(vm_host_map, key=lambda x: x['sd']['total_sd'])
-    print vm_host_map
+    pp.pprint(vm_host_map)
+
+
+def get_instance_resources(i):
+    instance_resources = {'uuid': i['instance_uuid']}
+    instance_resources['cpu'] = calculate_cpu(i)
+    instance_resources['memory'] = i['mem']
+    instance_resources['io'] = i[
+        'block_dev_iops'] - i['prev_block_dev_iops']
+    return instance_resources
+
 
 min_sd(nodes=compute_nodes)
+
+for i in instances:
+    instance_resources = get_instance_resources(i)
+    print instance_resources
